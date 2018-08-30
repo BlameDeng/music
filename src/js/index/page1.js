cc8360e502cf3b7629fcf021d00f4d48be3830cc
BlameDeng <476961779@qq.com>
2018-08-30 11:56:09 +0800

    添加了歌曲跳转播放功能

--- src/js/index/page1.js
+++ src/js/index/page1.js
@@ -1,7 +1,114 @@
-//let middle right
-
-setInterval(()=>{
-$('.left').addClass('right').removeClass('left');
-$('.middle').addClass('left').removeClass('middle');
-$('.right').addClass('middle').removeClass('right');
-},3000)
+{
+    let view = {
+        el: '.page1',
+        template: `<section class="banner">
+        <div class="slides">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide"><img src="./img/slide.png" alt=""></div>
+                    <div class="swiper-slide"><img src="./img/slide.png" alt=""></div>
+                    <div class="swiper-slide"><img src="./img/slide.png" alt=""></div>
+                    <div class="swiper-slide"><img src="./img/slide.png" alt=""></div>
+                    <div class="swiper-slide"><img src="./img/slide.png" alt=""></div>
+                </div>
+            </div>
+            <div class="recommendation active">
+                <div class="true">
+                    <div><img src="./img/art1.jpg" alt=""></div>
+                    <div><img src="./img/art2.png" alt=""></div>
+                    <div><img src="./img/art1.jpg" alt=""></div>
+                    <div><img src="./img/art2.png" alt=""></div>
+                </div>
+                <div class="fake">
+                    <div><img src="./img/art1.jpg" alt=""></div>
+                    <div><img src="./img/art2.png" alt=""></div>
+                    <div><img src="./img/art1.jpg" alt=""></div>
+                    <div><img src="./img/art2.png" alt=""></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="newSong">
+        <h2>最新音乐</h2>
+        <ol></ol>
+    </section>`,
+    templateLi:`<li>
+    <div class="songicon">
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-yinle"></use>
+        </svg>
+    </div>
+    <div class="songinfo">
+        <p>__name__</p>
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-geshou"></use>
+        </svg>
+        <span>__singer__</span>
+    </div>
+    <div class="play">
+        <a href="./play.html?id=__id__"> <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-Symbols4"></use>
+            </svg></a>
+    </div>
+</li>`,
+        render(data) {
+            $(this.el).html(this.template);
+        },
+        addLi(data){
+            let songs=data.songs;
+            songs.map((song)=>{
+                let {name,id,singer,url}=song;
+                let html=this.templateLi.replace('__name__',name).replace('__singer__',singer)
+                .replace('__id__',id);
+                let domLi=$(html);
+                $(this.el).find('ol').append(domLi);
+            });
+        }
+    };
+    let model = {
+        data: {
+            songs: []
+        },
+        fetch() {
+            var query = new AV.Query('Song');
+            return query.find().then(function (songs) {
+                songs.forEach(function (song) {
+                    song.set('status', 1);
+                });
+                return AV.Object.saveAll(songs);
+            }).then((songs) => { //songs对象数组 song有id和attributes属性
+                // 更新成功
+                songs.map((song) => {
+                    let id = song.id;
+                    let {
+                        name,
+                        url,
+                        singer
+                    } = song.attributes;
+                    let data = {
+                        name: name,
+                        singer: singer,
+                        url: url,
+                        id: id
+                    };
+                    this.data.songs.push(data);
+                });
+            }, function (error) {
+                // 异常处理
+            });
+        }
+    };
+    let controller = {
+        view: null,
+        model: null,
+        init(view, model) {
+            this.view = view;
+            this.model = model;
+            this.model.fetch().then(()=>{
+                this.view.addLi(this.model.data);
+            })
+            this.view.render(this.model.data);
+        }
+    };
+    controller.init(view, model);
+}
---
+++ src/js/play/play.js
@@ -0,0 +1,57 @@
+{
+    let view = {
+        el: '.app',
+        template: `<audio controls src="__url__"></audio>`,
+        render(data) {
+            console.log(data)
+            let url=data.attributes.url;
+            let html=this.template.replace('__url__',url);
+            $(this.el).html(html);
+        }
+    };
+    let model = {
+        data: {},
+    };
+    let controller = {
+        view: null,
+        model: null,
+        init(view, model) {
+            this.view = view;
+            this.model = model;
+            this.fetch().then(() => {
+                this.view.render(this.model.data);
+                
+            })
+        },
+        getId() {
+            let search = window.location.search;
+            if (search.indexOf('?') === 0) {
+                search = search.substring(1);
+            }
+            let array = search.split('&').filter((n) => {
+                return n;
+            })
+            let id = undefined;
+            for (let i = 0; i < array.length; i++) {
+                let key = array[i].split('=')[0];
+                let value = array[i].split('=')[1];
+                if (key === 'id') {
+                    id = value;
+                    break;
+                }
+            };
+            return id;
+        },
+        fetch() {
+            let query = new AV.Query('Song');
+            let id = this.getId();
+            return query.get(id).then((song)=> {
+                // song 就是 id 对象实例 attributes,id
+                this.model.data = song;
+            }, function (error) {
+                // 异常处理
+            });
+        }
+    };
+    controller.init(view, model);
+}
