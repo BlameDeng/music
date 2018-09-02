4491adae657543f5d552787d0c9d7bc8e7419b49
BlameDeng <476961779@qq.com>
2018-09-02 15:00:11 +0800

    添加首页点击导航跳转页面

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,23 +1,11 @@
 {
     let view = {
         el: '.top',
-        template: `<header>
-        <div class="logo">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="#icon-music"></use>
-            </svg>
-            <p>网易云音乐</p>
-        </div>
-        <div>下载APP</div>
-    </header>
-    <nav>
-        <ul class="topnav">
-            <li class="active">推荐</li>
-            <li class="">排行榜</li>
-            <li class="">搜索</li>
-        </ul>
-        <hr>
-    </nav>`,
+        template: `<header><div class="logo">
+<svg class="icon" aria-hidden="true"><use xlink:href="#icon-music"></use>
+</svg><p>网易云音乐</p></div><div>下载APP</div></header><nav>
+<ul class="topnav"><li class="active">推荐</li><li class="">排行榜</li>
+<li class="">搜索</li></ul><hr></nav>`,
         render(data) {
             $(this.el).html(this.template);
         }
@@ -35,6 +23,8 @@
         bindEvents() {
             $(this.view.el).on('click', 'li', (e) => {
                 $(e.currentTarget).addClass('active').siblings().removeClass('active');
+                let index = $(e.currentTarget).index();
+                $('.page').eq(index).addClass('active').siblings().removeClass('active');
             })
         }
     };
