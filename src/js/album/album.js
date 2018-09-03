b6eb9a0fd75a3fcfea87d6f2f48b877f545b7864
BlameDeng <476961779@qq.com>
2018-09-03 17:18:55 +0800

    album页基础构建

--- src/js/album/album.js
+++ src/js/album/album.js
@@ -1,1 +1,20 @@
-console.log(window.location.search);
+let search = window.location.search;
+if (search.indexOf('?') === 0) {
+    search = search.substring(1);
+};
+let array = search.split('&').filter((v) => v);
+let listId;
+for (let i = 0; i < array.length; i++) {
+    let arr = array[i].split('=');
+    if (arr[0] === 'listid') {
+        listId = arr[1];
+        break;
+    };
+};
+
+var query = new AV.Query('SongList');
+query.get(listId).then((songlist) => {
+    console.log(songlist);
+}, function (error) {
+    // 异常处理
+});
